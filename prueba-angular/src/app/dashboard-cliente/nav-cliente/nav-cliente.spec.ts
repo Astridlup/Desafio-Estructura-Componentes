c90b86b0379f8a37194958c8e7f7427970927a4c
astridlup <astrita1970@gmail.com>
2026-04-21 16:42:45 -0300

    Creacion estructura componentes angular

---
+++ prueba-angular/src/app/dashboard-cliente/nav-cliente/nav-cliente.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavCliente } from './nav-cliente';
+
+describe('NavCliente', () => {
+  let component: NavCliente;
+  let fixture: ComponentFixture<NavCliente>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NavCliente],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(NavCliente);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ prueba-angular/src/app/dashboard-cliente/nav-cliente/nav-cliente.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-cliente',
+  imports: [],
+  templateUrl: './nav-cliente.html',
+  styleUrl: './nav-cliente.css',
+})
+export class NavCliente {}
